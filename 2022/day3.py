3a50b03347c318d62407a953921cf6f50cae28ad
Mateusz Dukat <mateusz.dukat1@gmail.com>
2022-12-03 17:25:51 +0100

    Update day3.py

--- 2022/day3.py
+++ 2022/day3.py
@@ -21,7 +21,26 @@ for rucksack in open("data.txt", "r").readlines():
     for item in firstCompartment:
         if item in secondCompartment:
             prioritiesSum += itemToPriority(item)
-            print(firstCompartment + " " + secondCompartment + " = " + item + " (" + str(itemToPriority(item)) + ")")
+            # print(firstCompartment + " " + secondCompartment + " = " + item + " (" + str(itemToPriority(item)) + ")")
             break  # Only one item per line, ignore rest
 
-print(prioritiesSum)
+print("Part 1: " + str(prioritiesSum))
+
+#################################### PART 2 #########################################
+
+# Split rucksack list into 3s
+rucksackGroups = []
+prioritiesSum = 0
+
+data = open("data.txt", "r").readlines()
+
+for i in range(0, len(data), 3):
+    rucksackGroups.append(data[i:i+3])
+
+for rucksackGroup in rucksackGroups:  # For each group of 3 rucksacks
+    for item in rucksackGroup[0].replace('\n', ''):  # Fixme: replace for each string in group
+        if item in rucksackGroup[1].replace('\n', '') and item in rucksackGroup[2].replace('\n', ''):  # If any item in first rucksack is in second and third
+            prioritiesSum += itemToPriority(item)  # Add this items priority value
+            break  # Go to next group of 3s
+
+print("Part 2: " + str(prioritiesSum))
